f7b9ca160bb02ba8012e95186da1bd7768f1da8b
hiroki <gp19a036@oecu.jp>
2022-01-14 02:15:58 +0900

    コミット2

---
+++ kadai-1.2.py
@@ -0,0 +1,12 @@
+for num in range(1, 201):
+    if num % 3 == 0 and num % 15 != 0:
+        print("勉強したい")
+        continue
+    elif num % 5 == 0 and num % 15 != 0:
+        print("就活したい")
+        continue
+    elif num % 15 ==0:
+        print("だいがく爆破したい")
+        continue
+    else:
+        print(num)
